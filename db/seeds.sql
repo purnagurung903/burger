6ccbb67bd74bad144a86a16086de4adab3c71f73
Purna Gurung <purnagurung903@gmail.com>
2021-02-25 10:45:14 -0500

    added database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Quarterpounder");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers (burger_name) VALUES ("Hamburger");
+
