0ce84160067aa5b4ccb9569ff88e42d7de9fd1eb
Purna Gurung <purnagurung903@gmail.com>
2021-03-09 18:11:48 -0500

    fixed

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // set uo mysql connection
 const mysql = require("mysql");
 
-// var connection;
+var connection;
 
 
-// if(process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }
-// else {
-    const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "7Angelpa",
     database: "burgers_db",
   });
-//}
+}
 
 connection.connect((err) => {
   if (err) {
